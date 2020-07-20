a09eb86439b5471648af10cdb43a41281d38fb87
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-20 19:51:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,18 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@count = []
-
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-
+    @@count = []
+  
     @@count << self
 
   end
+
+
 
   def self.count
     @@count
