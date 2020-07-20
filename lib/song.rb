b1f65cc15e722f2245026cece024b88cf340e13e
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-20 20:06:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
 
   @@count = 0
   @@artists = []
-  
+
 
 
   def initialize(name, artist, genre)
@@ -22,7 +22,7 @@ class Song
 
   def self.count
     @@count
-    binding.pry
+    
 
   end
 
